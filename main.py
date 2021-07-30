c9146bfb0fc13f12b9c8e9b284e4fa306a378146
NolaDavid <dguillory62888@gmail.com>
2021-07-30 03:45:59 +0000

    adjusted the maximum index to not extend past whats available

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ names_string = input("Give me everybody's names, separated by a comma. ")
 names = names_string.split(", ")
 # 🚨 Don't change the code above 👆
 number = len(names)
-print(number)
 #Write your code below this line 👇
-person = names[random.randint(0, number)]
+person = names[random.randint(0, number - 1)]
 print(f"{person} is paying the bill today!")
 
 
